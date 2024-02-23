6ba33b263b1f831798f001471a1c15cf918db3da
Your Name <youremail@example.com>
2024-02-23 16:19:40 +0500

    jawan pakistan

---
+++ script.js
@@ -0,0 +1,178 @@
+
+
+
+// Chapter # 01
+
+// Q1 Write a script to greet your website visitor using JS alert box.
+
+alert("Welcome To Our Side....");
+
+// 2. Write a script to display following message on your web page
+
+alert("Error! Please enter a valid password.");
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+alert("Welcome to JS Land.. \n Happy Coding!")
+
+// 4. Write a script to display following messages in sequence:
+alert("Welcome to JS Land.."); 
+alert("Happy Coding!");
+
+// 5. Generate the following message through browser’s
+// developer console:
+
+alert("Hello... I can run JS through my web browser's console");
+
+// chapter # 02
+
+// 1. Declare a variable called username    
+
+var username = "baseer123";
+
+// 2. Declare a variable called myName & assign to it a string 
+// that represents your Full Name.
+var myName = "Abdul baseer Vohra";
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+var message = "Hello World";
+alert(message);
+
+
+// 4. Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes.
+
+
+var firstName = "Abdul baseer";
+var edu = "Certified WEB AND MOBILE HYBRID APPLICATION DEVELOPER";
+var age = "23 years old";
+
+alert(firstName);
+alert(edu);
+alert(age);
+
+// 5. Write a script to display the following alert using one JS 
+// variable
+
+var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+
+alert(pizza);
+
+// 6. Declare a variable called email and assign to it a string that 
+// represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use 
+// string concatenation)
+
+var email = "abaseer4321@gmail.com";
+alert("My email address is " + email);
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+var Book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + Book);
+
+// 8. Write a script to display this in browser through JS
+
+document.write("Yah! I can write HTML content through Javascript.");
+
+// 9. Store following string in a variable and show in alert and 
+// browser through JS
+var design ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+
+// Chapter # 03
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+var age = 21;
+alert("I am " + age +" year old ")
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”
+
+var N = 14;
+alert("You have visited this site" + N +" times");
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+var birthYear = 2000;
+document.write("My Birth Year is " + birthYear + "<br /> Data type of my declared variable is " + typeof birthYear +"<br />");
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”
+
+var visitorsName = "John Doe";
+var productTitle = "XYZ Clothing store";
+var quantity = "5 T-shirt(s)";
+
+document.write("<b>"+ visitorsName + "</b> ordered <b>" + quantity +"</b>" + productTitle);
+
+
+//chapter 3-1
+
+//chapter 1
+//q1
+var firstName = "Abdul";
+var lastName = "Baseer";
+var email = "abseer4321@gmail.com";
+var phoneNumber = "123-456-7890";
+var password = "securepassword";
+
+// Alert each piece of information individually
+alert("First Name: " + firstName);
+alert("Last Name: " + lastName);
+alert("Email: " + email);
+alert("Phone Number: " + phoneNumber);
+alert("Password: " + password);
+
+//q2
+alert("your are learning javascript");
+
+//q3
+alert("hello world")
+
+//chapter 2
+var myVariableName = "abdul Baseer";
+
+var myvariable;
+var myvariable2 = "baseer";
+
+
+var teamname = "Pakistancricketteam"
+alert("Team name" + teamname)
+
+var bestMan = "BASEER"; 
+bestMan = "VOHRA"; 
+
+//Chapter 3
+
+var caseQty;
+ caseQty = "144";
+ var num = "9"
+
+ var sumResult;
+sumResult = 2 + 3;
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+var num = 9;
+
+num = num + 4 ; 
